6d602ed22c18aab8fecece1698d6e065754d610f
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-06-26 22:36:49 -0300

    chore: installing and configuring the corrects dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  clearMocks: true,
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*ts'],
   coverageDirectory: 'coverage',
+  preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
     '.+\\test.ts$': 'ts-jest'
