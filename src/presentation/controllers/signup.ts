71b8719c9e5a79a1035d63a2e0690a633fd1ae91
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-07-15 12:33:40 -0300

    feat: Adicionando validação de email no SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,29 @@
-import { MissingParamError } from '../errors/missingParamError';
-import { type HttpResponse, type HttpRequest } from '../protocols/http';
+import { MissingParamError } from '../errors/MissingParamError';
+import { type HttpResponse, type HttpRequest } from '../protocols/Http';
 import { badRequest } from '../helpers/httpHelper';
-import { Controller } from '../protocols/controller';
+import { Controller } from '../protocols/Controller';
+import { EmailValidator } from '../protocols/EmailValidator';
+import { InvalidParamError } from '../errors/InvalidParamError';
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
     for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
             return badRequest(new MissingParamError(field));
         }
+    }
+
+    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
+
+    if (!isValidEmail) {
+      return badRequest(new InvalidParamError('email'));
     }
 
     return {
