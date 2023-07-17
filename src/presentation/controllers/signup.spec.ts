a6316bed47b79d5983a691f9071958ca397ba7df
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-07-17 20:43:53 -0300

    refactor: movendo import de erros para arquivo único

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,6 @@
 import { SignUpController } from './signup';
-import { MissingParamError } from '../errors/MissingParamError';
-import { InvalidParamError } from '../errors/InvalidParamError';
+import { ServerError, MissingParamError, InvalidParamError } from '../errors';
 import { EmailValidator } from '../protocols/EmailValidator';
-import { ServerError } from '../errors/ServerError';
 
 interface SutTypes {
   sut: SignUpController
