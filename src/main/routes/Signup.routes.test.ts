73ec312d3e5aa624f20879fb53518609e0f2986c
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-08-22 20:28:54 -0300

    refactor: conectando ao mongodb antes de teste de integração

--- src/main/routes/Signup.routes.test.ts
+++ src/main/routes/Signup.routes.test.ts
@@ -1,7 +1,25 @@
 import request from 'supertest';
 import app from '../config/app';
+import * as dotenv from 'dotenv';
+import { MongoHelper } from '../../infra/criptography/db/mongoDb/helpers/mongoHelper';
 
 describe('SignUp Routes', () => {
+    dotenv.config();
+    const ACCOUNTS_COLLECTION_TEST = process.env.ACCOUNTS_COLLECTION_TEST as string;
+
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL as string);
+    });
+
+    afterAll(async () => {
+        await MongoHelper.disconnect();
+    });
+
+    beforeEach(async () => {
+        const accountCollection = MongoHelper.getCollection(ACCOUNTS_COLLECTION_TEST);
+        await accountCollection.deleteMany({});
+    });
+
     test('Should return an account on sucess', async () => {
         await request(app)
             .post('/api/signup')
