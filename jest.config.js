b939192fffef6179d3d462ed85ce4ad3306e2d42
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-08-21 21:20:36 -0300

    chore: corrigindo arquivos de configuração

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  clearMocks: true,
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*ts', '!<rootDir>/src/**/*.protocols.ts', '!<rootDir>/src/**/index.ts'],
-  coverageDirectory: 'coverage',
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\test.ts$': 'ts-jest'
-  }
+    clearMocks: true,
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: ['<rootDir>/src/**/*ts', '!<rootDir>/src/**/*.protocols.ts', '!<rootDir>/src/**/index.ts'],
+    coverageDirectory: 'coverage',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+        '.+\\test.ts$': 'ts-jest'
+    }
 };
