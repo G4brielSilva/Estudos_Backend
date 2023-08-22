a7c12252dcf8bcfce6577264700f3bbff6fd9800
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-08-21 22:10:18 -0300

    feat: garantindo que cors está ativo

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlewares/bodyParser/BodyParser';
+import { cors } from '../middlewares/cors/Cors';
 
 export default (app: Express): void => {
     app.use(bodyParser);
+    app.use(cors);
 };
