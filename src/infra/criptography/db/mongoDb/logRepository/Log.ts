3267fb40ac99f76df3c3c6c342ceb66485b10670
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-08-28 17:24:39 -0300

    feat: adicionando o LogMongoRepository ao signup factory

--- src/infra/criptography/db/mongoDb/logRepository/Log.ts
+++ src/infra/criptography/db/mongoDb/logRepository/Log.ts
@@ -7,7 +7,6 @@ dotenv.config();
 export class LogMongoRepository implements LogErrorRepository {
     async logError(stack: string): Promise<void> {
         const errorCollection = await MongoHelper.getCollection(process.env.ERRORS_COLLECTION as string);
-
         await errorCollection.insertOne({
             stack,
             date: new Date()
